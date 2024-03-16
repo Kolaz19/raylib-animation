8477bf63dc0c22ed7aaa9ec20b3cb429556f2f94
Nikolas <etc.nikolas@gmail.com>
2024-03-16 11:02:20 +0100

    Fix raylib template

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main(void)
     while (!WindowShouldClose())
     {
         BeginDrawing();
+            ClearBackground(RAYWHITE);
             DrawText("Template", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
