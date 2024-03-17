367d2dc3c4b526660fd2ca61150f32b20c68d5cb
Nikolas <etc.nikolas@gmail.com>
2024-03-17 20:35:01 +0100

    Create and play animations

--- animation.c
+++ animation.c
@@ -1,25 +1,64 @@
 #include "animation.h"
+#include "include/raylib.h"
 
-struct Spritesheet {
-    Image* image;
-    int amountFrames;
-    int frameWidth;
-    int frameHeight;
-};
+Spritesheet LoadSpritesheet(const char* fileName, int frameWidth, int frameHeight) {
+    Spritesheet spriteSheet;
 
-Spritesheet LoadSpritesheet(const char* fileName, int amountFrames, int frameWidth, int frameHeight) {
-    Spritesheet sh;
+    Texture2D texture = LoadTexture(fileName);
+    spriteSheet.texture = texture;
 
-    Image img = LoadImage(fileName);
-    sh.image = &img;
+    spriteSheet.frameWidth = frameWidth;
+    spriteSheet.frameHeight = frameHeight;
 
-    sh.amountFrames = amountFrames;
-    sh.frameWidth = frameWidth;
-    sh.frameHeight = frameHeight;
-
-    return sh;
+    return spriteSheet;
 }
 
-void unloadSpritesheet(Spritesheet spriteSheet) {
-    UnloadImage(*(spriteSheet.image));
+void unloadSpritesheet(Spritesheet* spriteSheet) {
+    UnloadTexture((spriteSheet->texture));
+}
+
+Animation createAnimation(Spritesheet* spriteSheet, int startFrame, int endFrame, float frameDuration, AnimationType type) {
+    Animation animation;
+    animation.spriteSheet = spriteSheet;
+    animation.startFrame = startFrame;
+    animation.endFrame = endFrame;
+    animation.currentFrame = startFrame;
+    animation.frameDuration = frameDuration;
+    animation.advancedTime = 0;
+    animation.type = type;
+    animation.isPlaying = type == LOOP ? true : false; 
+
+    return animation;
+}
+
+void advanceAnimation(Animation* animation) {
+    if (!animation->isPlaying) {
+	return;
+    }
+
+    animation->advancedTime += GetFrameTime();
+    if (animation->advancedTime >= animation->frameDuration) {
+	animation->advancedTime = 0;
+	animation->currentFrame++;
+    }
+
+    //Loop back to start frame
+    if (animation->currentFrame > animation->endFrame) {
+	animation->currentFrame = animation->startFrame;
+	if (animation->type == PLAY_ONCE) {
+	    animation->isPlaying = false;
+	}
+    }
+}
+
+void drawAnimation(Animation* animation, float posX, float posY, float width, float height, float originX,float originY, float rotation ) {
+    if (!animation->isPlaying) {
+	return;
+    }
+
+    int amountFramesX = animation->spriteSheet->texture.width / animation->spriteSheet->frameWidth;
+    int yPos = (animation->currentFrame - 1) / amountFramesX * animation->spriteSheet->frameHeight;
+    int xPos = (animation->currentFrame - 1 - ((animation->currentFrame / amountFramesX) * amountFramesX)) * animation->spriteSheet->frameWidth;
+    Rectangle source = { xPos, yPos, animation->spriteSheet->frameWidth, animation->spriteSheet->frameHeight};
+    DrawTexturePro(animation->spriteSheet->texture, source, (Rectangle){posX,posY, width,height}, (Vector2) {originX,originY}, rotation,  WHITE);
 }
--- animation.h
+++ animation.h
@@ -4,8 +4,34 @@
 #include "include/raylib.h"
 
 typedef struct Spritesheet Spritesheet;
-Spritesheet LoadSpritesheet(const char* fileName, int amountFrames, int frameWidth, int frameHeight);
-void unloadSpritesheet(Spritesheet spriteSheet);
+typedef struct Animation Animation;
 
+struct Spritesheet {
+    Texture2D texture;
+    int frameWidth;
+    int frameHeight;
+};
 
+typedef enum AnimationType {
+    LOOP,
+    PLAY_ONCE
+} AnimationType;
+
+struct Animation {
+    Spritesheet* spriteSheet;
+    int startFrame;
+    int endFrame;
+    int currentFrame;
+    float frameDuration;
+    float advancedTime;
+    AnimationType type;
+    bool isPlaying;
+};
+
+Spritesheet LoadSpritesheet(const char* fileName,  int frameWidth, int frameHeight);
+void unloadSpritesheet(Spritesheet* spriteSheet);
+
+Animation createAnimation(Spritesheet* spriteSheet, int startFrame, int endFrame, float frameDuration, AnimationType type);
+void drawAnimation(Animation* animation, float posX, float posY, float width, float height, float originX,float originY, float rotation );
+void advanceAnimation(Animation* animation);
 #endif
